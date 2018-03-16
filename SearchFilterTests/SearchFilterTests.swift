574f2c19b3658de14d28e596e197056ab81937f1
Patrick Marshall <patrick@gits.co.id>
2018-03-16 13:50:58 +0700

    Initial Commit

---
+++ SearchFilterTests/SearchFilterTests.swift
@@ -0,0 +1,36 @@
+//
+//  SearchFilterTests.swift
+//  SearchFilterTests
+//
+//  Created by Patrick Marshall on 3/16/18.
+//  Copyright © 2018 Patrick Marshall. All rights reserved.
+//
+
+import XCTest
+@testable import SearchFilter
+
+class SearchFilterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
